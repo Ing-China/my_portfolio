4f06d04559ce7892ba66a628d699abe2363d9557
Ing-China <peschina6677@gmail.com>
2025-03-09 23:29:57 +0700

    Working on Hero section (50% | +50%)

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,46 @@
+import { useEffect } from "react";
+import obj from "../assets/obj1.png";
+import {
+  animate,
+  useMotionTemplate,
+  useMotionValue,
+  motion,
+} from "framer-motion";
+
+const COLORS_TOP = ["#13FFAA", "#1E67C6", "#CE84CF", "#DD335C"];
 const Hero = () => {
-  return <></>;
+  const color = useMotionValue(COLORS_TOP[0]);
+
+  useEffect(() => {
+    animate(color, COLORS_TOP, {
+      ease: "easeInOut",
+      duration: 10,
+      repeat: Infinity,
+      repeatType: "mirror",
+    });
+  }, []);
+
+  const backgroundImage = useMotionTemplate`radial-gradient(125% 125% at 50% 0%, #000 50%, ${color})`;
+  const border = useMotionTemplate`1px solid ${color}`;
+  const boxShadow = useMotionTemplate`0px 4px 24px ${color}`;
+  return (
+    <motion.section
+      style={{
+        backgroundImage,
+      }}
+      className="relative grid min-h-screen place-content-center overflow-hidden px-4 py-24 text-gray-200 "
+    >
+      <div className="z-10 flex flex-col items-center">
+        <span className="mb-1.5 inline-block rounded-full bg-gray-600/50 px-3 py-1.5 text-sm">
+          Open for work
+        </span>
+        <h1 className="text-white/40 text-7xl font-black">Hi, I am</h1>
+        <h1 className="max-w-3xl bg-gradient-to-br from-white to-gray-400 bg-clip-text font-black leading-tight text-transparent md:text-7xl">
+          Ing China
+        </h1>
+      </div>
+    </motion.section>
+  );
 };
 
 export default Hero;
