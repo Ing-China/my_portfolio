701c08c0cc14455eb9999ebcfe68104a45955972
Ing-China <peschina6677@gmail.com>
2025-03-10 11:41:12 +0700

    Coding on section Hero (100% | +50%)

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,11 +1,14 @@
 import { useEffect } from "react";
 import obj from "../assets/obj1.png";
+import profilepic from "../assets/profilepic.png";
 import {
   animate,
   useMotionTemplate,
   useMotionValue,
   motion,
 } from "framer-motion";
+import Image from "next/image";
+import { FiArrowRight } from "react-icons/fi";
 
 const COLORS_TOP = ["#13FFAA", "#1E67C6", "#CE84CF", "#DD335C"];
 const Hero = () => {
@@ -34,10 +37,38 @@ const Hero = () => {
         <span className="mb-1.5 inline-block rounded-full bg-gray-600/50 px-3 py-1.5 text-sm">
           Open for work
         </span>
-        <h1 className="text-white/40 text-7xl font-black">Hi, I am</h1>
-        <h1 className="max-w-3xl bg-gradient-to-br from-white to-gray-400 bg-clip-text font-black leading-tight text-transparent md:text-7xl">
+        <h1 className="text-white/40 text-5xl md:text-7xl font-black">
+          Hi, I am
+        </h1>
+        <h1 className="max-w-3xl bg-gradient-to-br from-white to-gray-400 bg-clip-text font-black leading-tight text-transparent text-5xl md:text-7xl">
           Ing China
         </h1>
+        <Image src={profilepic} alt="profile pic" width={200} />
+
+        <div className="flex bg-white/10 shadow-xl p-3 rounded-3xl justify-center items-center space-x-2 mb-4">
+          <Image src={obj} alt="obj" width={30} className="rounded-2xl" />
+          <Image src={obj} alt="obj" width={30} className="rounded-2xl" />
+          <Image src={obj} alt="obj" width={30} className="rounded-2xl" />
+          <p className="font-medium">80+ Happy Clients</p>
+        </div>
+        <p className="my-6 max-w-xl text-center">
+          Fullstack Developer based in Siem Reap, with over 2 years of
+          experience
+        </p>
+
+        <motion.button
+          style={{ border, boxShadow }}
+          whileHover={{ scale: 1.015 }}
+          whileTap={{ scale: 0.985 }}
+          className="flex w-fit items-center gap-2 rounded-full px-4 py-2"
+        >
+          Download CV
+          <FiArrowRight />
+        </motion.button>
+      </div>
+      <div className="bg-circle-container">
+        <div className="bg-circle-background"></div>
+        <div className="bg-circle"></div>
       </div>
     </motion.section>
   );
