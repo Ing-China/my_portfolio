cc36efa8513c816b186f0e229ac1f944804f7210
Ing-China <peschina6677@gmail.com>
2025-03-11 15:15:30 +0700

    Coding on Stack section (100% | +50%), Services section (100% | +100%), LogoAnimation section (100% | +100%), KeyMetrics section (100% | +100%)

--- components/Stack.tsx
+++ components/Stack.tsx
@@ -7,10 +7,10 @@ import { TbBrandNextjs } from "react-icons/tb";
 
 const stackItems = [
   { id: 1, name: "React", icon: FaReact, color: "#61DAFB" },
-  { id: 2, name: "Node.js", icon: IoLogoNodejs, color: "#61DAFB" },
-  { id: 3, name: "TypeScript", icon: SiTypescript, color: "#61DAFB" },
-  { id: 4, name: "MongoDB", icon: SiMongodb, color: "#61DAFB" },
-  { id: 5, name: "Next.js", icon: TbBrandNextjs, color: "#61DAFB" },
+  { id: 2, name: "Node.js", icon: IoLogoNodejs, color: "#339933" },
+  { id: 3, name: "TypeScript", icon: SiTypescript, color: "#3178C6" },
+  { id: 4, name: "MongoDB", icon: SiMongodb, color: "#47A248" },
+  { id: 5, name: "Next.js", icon: TbBrandNextjs, color: "#000000" },
 ];
 
 const Stack = () => {
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,19 @@
 import Hero from "@/components/Hero";
+import KeyMetrics from "@/components/KeyMetrics";
+import LogoAnimation from "@/components/LogoAnimation";
 import Navbar from "@/components/Navbar";
+import Services from "@/components/Services";
 import Stack from "@/components/Stack";
-
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      <Hero/>
-      <Stack/>
+      <Hero />
+      <Stack />
+      <LogoAnimation />
+      <KeyMetrics />
+      <Services />
     </>
   );
 }
