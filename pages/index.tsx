8482283382ce2a98eb444e02cd2d7cccca4fc865
Ing-China <peschina6677@gmail.com>
2025-03-11 07:49:44 +0700

    Working on Stack section (50% | +50%)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,14 @@
 import Hero from "@/components/Hero";
-// import Navbar from "@/components/Navbar";
+import Navbar from "@/components/Navbar";
+import Stack from "@/components/Stack";
+
 
 export default function Home() {
   return (
     <>
-      {/* <Navbar /> */}
+      <Navbar />
       <Hero/>
+      <Stack/>
     </>
   );
 }
